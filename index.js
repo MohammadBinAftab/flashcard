f39dc7aff1f3cd74f5e83f1a30b8257a9b2b58d3
MohammadBinAftab <48010758+MohammadBinAftab@users.noreply.github.com>
2024-08-13 20:29:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(
+  cors({
+    origin: ["https://flashcard-seven-beta.vercel.app/", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(express.json());
+
+const db = mysql.createPool({
+  host: 'bxi0fter7gq2y5rkk80a-mysql.services.clever-cloud.com',
+  user: 'uzw4h2eoru3gb4xj', 
+  password: 'BEzrKoFxAwm0zSzF5FYc',
+  database: 'bxi0fter7gq2y5rkk80a',
+});
+
+// Get all flashcards
+app.get('/flashcards', (req, res) => {
+  db.query('SELECT * FROM flashcards', (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+// Add a new flashcard
+app.post('/flashcards', (req, res) => {
+  const { question, answer } = req.body;
+  db.query('INSERT INTO flashcards (question, answer) VALUES (?, ?)', [question, answer], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ id: result.insertId, question, answer });
+  });
+});
+
+// Update an existing flashcard
+app.put('/flashcards/:id', (req, res) => {
+  const { id } = req.params;
+  const { question, answer } = req.body;
+  db.query('UPDATE flashcards SET question = ?, answer = ? WHERE id = ?', [question, answer, id], (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.sendStatus(200);
+  });
+});
+
+// Delete a flashcard
+app.delete('/flashcards/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM flashcards WHERE id = ?', [id], (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.sendStatus(200);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
