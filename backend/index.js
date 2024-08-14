d8b96774ee39d2ce3f6d369e08a4e0bb89ea45cb
MohammadBinAftab <48010758+MohammadBinAftab@users.noreply.github.com>
2024-08-14 15:31:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'https://flashcard1.vercel.app',
+  origin: ["https://flashcard1.vercel.app", "https://flashcard1.vercel.app/admin"],
 }));
 app.use(express.json());
 
